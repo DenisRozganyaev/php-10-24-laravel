d406c053606f51dde5e1a8a6a739b5c89b0f1f30
apple <denis.rozganyaev@gmail.com>
2025-01-07 23:22:09 +0200

    Admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +9,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin|moderator'])->group(function () {
+    Route::get('/', DashboardController::class)->name('dashboard'); // domain/admin/ | admin.dashboard
+    Route::resource('categories', \App\Http\Controllers\Admin\CategoriesController::class)
+        ->except(['show']);
+});
